f86398754a348c0e2006ebea0f43fa3449973a94
Lucia Widyana <mo_cia@yahoo.com>
2022-08-12 09:44:45 +0700

    initial

---
+++ src/Infrastructure.External.DanLirisClient.Microservice/MasterResult/PEBResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.External.DanLirisClient.Microservice.MasterResult
+{
+    public class PEBResult : BaseResult
+    {
+        public PEBResult()
+        {
+            data = new List<PEBResultViewModel>();
+        }
+        public IList<PEBResultViewModel> data { get; set; }
+    }
+
+    public class SinglePEBResultResult : BaseResult
+    {
+        public PEBResultViewModel data { get; set; }
+    }
+
+    public class PEBResultViewModel
+    {
+        public string BCNo { get; set; }
+        public string BonNo { get; set; }
+        public DateTime BCDate { get; set; }
+    }
+}
