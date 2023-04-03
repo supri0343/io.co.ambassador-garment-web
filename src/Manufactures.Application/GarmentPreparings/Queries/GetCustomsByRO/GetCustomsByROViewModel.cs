7e19768fe00bad493ea572cff847ed9bcc52d35c
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-04-03 14:53:14 +0700

    enhance-aval-with-fasilitas

---
+++ src/Manufactures.Application/GarmentPreparings/Queries/GetCustomsByRO/GetCustomsByROViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Application.GarmentPreparings.Queries.GetCustomsByRO
+{
+    public class GetCustomsByROViewModel
+    {
+        public List<GetCustomsByRODto> getCustomsByRO { get; set; }
+    }
+}
