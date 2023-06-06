6ea1deaa5eaa229ac733040fac70acbe54d9d165
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-06-06 11:03:16 +0700

    BugFix-LoaderLeftoverWareHouse-ExpenditureGood
    -add update isReceived G-SampleExpeditureGood

---
+++ src/Manufactures.Domain/GarmentSample/SampleExpenditureGoods/Commands/UpdateIsReceivedGarmentSampleExpenditureGoodCommand.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Domain.Commands;
+using Manufactures.Domain.GarmentExpenditureGoods.ValueObjects;
+using Manufactures.Domain.GarmentSample.SampleExpenditureGoods;
+using Manufactures.Domain.Shared.ValueObjects;
+using Microsoft.AspNetCore.JsonPatch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Manufactures.Domain.GarmentSampleExpenditureGoods.Commands
+{
+    public class UpdateIsReceivedGarmentSampleExpenditureGoodCommand : ICommand<GarmentSampleExpenditureGood>
+    {
+        public UpdateIsReceivedGarmentSampleExpenditureGoodCommand(Guid id, bool isReceived)
+        {
+            Identity = id;
+            IsReceived =isReceived;
+        }
+
+        public Guid Identity { get; private set; }
+        public bool IsReceived { get; private set; }
+    }
+}
