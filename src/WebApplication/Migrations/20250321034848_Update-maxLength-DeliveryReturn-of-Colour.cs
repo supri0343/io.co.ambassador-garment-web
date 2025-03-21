facd407243b3e053e838c0a54358fea0c2632b2d
supri0343 <63761987+supri0343@users.noreply.github.com>
2025-03-21 10:50:06 +0700

    2025/06/Fix/DeliveryReturn
    - fix max length of colour

---
+++ src/WebApplication/Migrations/20250321034848_Update-maxLength-DeliveryReturn-of-Colour.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DanLiris.Admin.Web.Migrations
+{
+    public partial class UpdatemaxLengthDeliveryReturnofColour : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Colour",
+                table: "GarmentDeliveryReturnItems",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Colour",
+                table: "GarmentDeliveryReturnItems",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+    }
+}
